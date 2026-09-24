a0e3ae850b951f1fe8161b7922eded5877a64754
kristinaperez <kristina.perez.28523@gmail.com>
2026-09-24 15:12:10 +0200

    Telegram Support Bot fixed

--- EspanolStarsCodex-support/src/app/learn/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from "react";
-import { AppShell } from "@/components/layout/app-shell";
-
-export default function LearnLayout({ children }: { children: ReactNode }) {
-  return <AppShell>{children}</AppShell>;
-}
